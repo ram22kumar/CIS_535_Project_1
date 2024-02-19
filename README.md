0cd08e26769c97b268dfbe341913fcdd3b2a42de
Ram Kumar <ramkumar2292000@gmail.com>
2024-02-18 21:23:49 -0500

    Added gyroscop screen and graph too

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# vscode_flutter_1
+# CIS535 Project - 1 : Sensors on Mobile Devices
 
-A new Flutter project.
+This project is my submission towards my assignment in the 1st Term which had three requirements.
+
+1. Upon opening the App you should see two buttons each pointing to different sensors.
+2. One among them should be Accelerometer and another one could be anything (I choose Gyroscope).
+3. On clicking either of the button should plot the values, mean, and variance on a graph.
+   NOTE: The values, mean, variance should be plotted against time and with taking the values using this formula below.
+
+```java
+value = sqrt(pow(X,2)+pow(Y,2)+pow(Z,2))
+```
+
+I have specifically used flutter as I wanted to try learning since I got the chance too. I have used sensor_plus, fl_chart for reading the data and plotting on the graph.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:light_sensor/light_sensor.dart';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -64,22 +65,34 @@ class AccelerometerScreen extends StatefulWidget {
 }
 
 class _AccelerometerScreenState extends State<AccelerometerScreen> {
-  List<FlSpot> dataPoints = [];
-  List<FlSpot> meanPoints = [];
-  List<FlSpot> variancePoints = [];
+  List<FlSpot> dataPoints = [const FlSpot(0, 0)];
+  List<FlSpot> meanPoints = [const FlSpot(0, 0)];
+  List<FlSpot> variancePoints = [const FlSpot(0, 0)];
   double maxVal = -99;
   late List<double> mainValues;
   List<AccelerometerEvent> _accelerometerEvents = [];
   late StreamSubscription<AccelerometerEvent> _accelerometerSubscription;
+  bool showLoader = false;
 
   double _accelerometerMainValueEvent(AccelerometerEvent event) {
-    double value = sqrt(
-        event.x * event.x + event.y * event.y * event.y + event.z + event.z);
+    double value =
+        sqrt(event.x * event.x + event.y * event.y + event.z + event.z);
     if (value > (maxVal / 2)) {
       maxVal = value * 2;
     }
-    dataPoints.add(FlSpot(dataPoints.length.toDouble(), value));
     return value;
+  }
+
+  void checkLimit(double limit) {
+    if (meanPoints.last.y >= limit) {
+      setState(() {
+        showLoader = true;
+      });
+    } else {
+      setState(() {
+        showLoader = false;
+      });
+    }
   }
 
   double _accelerometerMeanValueEvent(List<AccelerometerEvent> events) {
@@ -94,7 +107,6 @@ class _AccelerometerScreenState extends State<AccelerometerScreen> {
     double meanZ = sumZ / events.length;
 
     double mean = sqrt(meanX * meanX + meanY * meanY + meanZ * meanZ);
-    meanPoints.add(FlSpot(meanPoints.length.toDouble(), mean));
     return mean;
   }
 
@@ -129,7 +141,6 @@ class _AccelerometerScreenState extends State<AccelerometerScreen> {
 
     double variance = sqrt(
         varianceX * varianceX + varianceY * varianceY + varianceZ * varianceZ);
-    variancePoints.add(FlSpot(variancePoints.length.toDouble(), variance));
     return variance;
   }
 
@@ -139,18 +150,22 @@ class _AccelerometerScreenState extends State<AccelerometerScreen> {
     _accelerometerSubscription = accelerometerEventStream().listen((event) {
       setState(() {
         Future.delayed(const Duration(milliseconds: 100), () {
-          // Timer.periodic(Duration(milliseconds: 100), (timer) {
           if (!event.x.isNaN || !event.y.isNaN || !event.z.isNaN) {
             _accelerometerEvents.add(event);
-            _accelerometerMainValueEvent(event);
-            _accelerometerMeanValueEvent(_accelerometerEvents);
-            _accelerometerVarianceValueEvent(event, _accelerometerEvents);
+            double mainValue = _accelerometerMainValueEvent(event);
+            dataPoints.add(FlSpot(dataPoints.length.toDouble(), mainValue));
+            double meanValue =
+                _accelerometerMeanValueEvent(_accelerometerEvents);
+            meanPoints.add(FlSpot(dataPoints.length.toDouble(), meanValue));
+            double varianceValue =
+                _accelerometerVarianceValueEvent(event, _accelerometerEvents);
+            variancePoints
+                .add(FlSpot(variancePoints.length.toDouble(), varianceValue));
+            checkLimit(mainValue);
           }
         });
       });
     });
-    // _accelerometerSubscription = accelerometerEvents.listen((event) {
-    // });
   }
 
   @override
@@ -200,7 +215,7 @@ class _AccelerometerScreenState extends State<AccelerometerScreen> {
                   ]))),
         ),
         Padding(
-          padding: EdgeInsets.all(15),
+          padding: const EdgeInsets.all(15),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -217,22 +232,195 @@ class _AccelerometerScreenState extends State<AccelerometerScreen> {
                   style: const TextStyle(color: Colors.blue)),
             ],
           ),
-        )
+        ),
+        if (showLoader)
+          Center(
+            child: Container(
+              width: 10,
+              height: 10,
+              child: const CircularProgressIndicator(),
+            ),
+          ),
       ]),
     );
   }
 }
 
-class GyroscopeScreen extends StatelessWidget {
+class GyroscopeScreen extends StatefulWidget {
+  @override
+  _GyroscopeScreenState createState() => _GyroscopeScreenState();
+}
+
+class _GyroscopeScreenState extends State<GyroscopeScreen> {
+  List<FlSpot> dataPoints = [const FlSpot(0, 0)];
+  List<FlSpot> meanPoints = [const FlSpot(0, 0)];
+  List<FlSpot> variancePoints = [const FlSpot(0, 0)];
+  double maxVal = -99;
+  late List<double> mainValues;
+  List<GyroscopeEvent> _gyroscopeEvents = [];
+  late StreamSubscription<GyroscopeEvent> _gyroscopeSubscription;
+  bool showLoader = false;
+
+  double _gyroscopeMainValueEvent(GyroscopeEvent event) {
+    double value =
+        sqrt(event.x * event.x + event.y * event.y + event.z + event.z);
+    if (value > (maxVal / 2)) {
+      maxVal = value * 2;
+    }
+    return value;
+  }
+
+  void checkLimit(double limit) {
+    if (meanPoints.last.y >= limit) {
+      setState(() {
+        showLoader = true;
+      });
+    } else {
+      setState(() {
+        showLoader = false;
+      });
+    }
+  }
+
+  double _gyroscopeMeanValueEvent(List<GyroscopeEvent> events) {
+    double sumX = 0.0, sumY = 0.0, sumZ = 0.0;
+    for (var event in events) {
+      sumX += event.x;
+      sumY += event.y;
+      sumZ += event.z;
+    }
+    double meanX = sumX / events.length;
+    double meanY = sumY / events.length;
+    double meanZ = sumZ / events.length;
+
+    double mean = sqrt(meanX * meanX + meanY * meanY + meanZ * meanZ);
+    return mean;
+  }
+
+  double _gyroscopeVarianceValueEvent(
+      GyroscopeEvent mainEvent, List<GyroscopeEvent> events) {
+    double sumSqDiffX = 0.0;
+    double sumSqDiffY = 0.0;
+    double sumSqDiffZ = 0.0;
+
+    double meanEventsX = 0;
+    double meanEventsY = 0;
+    double meanEventsZ = 0;
+
+    for (var event in events) {
+      meanEventsX += event.x;
+      meanEventsY += event.y;
+      meanEventsZ += event.z;
+    }
+    meanEventsX /= events.length;
+    meanEventsY /= events.length;
+    meanEventsZ /= events.length;
+
+    for (var event in events) {
+      sumSqDiffX += pow((event.x - meanEventsX), 2);
+      sumSqDiffY += pow((event.y - meanEventsY), 2);
+      sumSqDiffZ += pow((event.z - meanEventsZ), 2);
+    }
+
+    double varianceX = sumSqDiffX / events.length;
+    double varianceY = sumSqDiffY / events.length;
+    double varianceZ = sumSqDiffZ / events.length;
+
+    double variance = sqrt(
+        varianceX * varianceX + varianceY * varianceY + varianceZ * varianceZ);
+    return variance;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _gyroscopeSubscription = gyroscopeEventStream().listen((event) {
+      setState(() {
+        Future.delayed(const Duration(milliseconds: 100), () {});
+        if (!event.x.isNaN || !event.y.isNaN || !event.z.isNaN) {
+          _gyroscopeEvents.add(event);
+          double mainValue = _gyroscopeMainValueEvent(event);
+          dataPoints.add(FlSpot(dataPoints.length.toDouble(), mainValue));
+          double meanValue = _gyroscopeMeanValueEvent(_gyroscopeEvents);
+          meanPoints.add(FlSpot(dataPoints.length.toDouble(), meanValue));
+          double varianceValue =
+              _gyroscopeVarianceValueEvent(event, _gyroscopeEvents);
+          variancePoints
+              .add(FlSpot(variancePoints.length.toDouble(), varianceValue));
+          checkLimit(mainValue);
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gyroscope Screen'),
+        title: const Text('Gyroscope Screen'),
       ),
-      body: Center(
-        child: Text('Gyroscope data will be displayed here'),
-      ),
+      body: Column(children: [
+        Expanded(
+          child: Container(
+              padding: const EdgeInsets.all(15),
+              child: LineChart(LineChartData(
+                  minX: 0,
+                  maxX: dataPoints.length.toDouble() + 10,
+                  minY: 0,
+                  maxY: maxVal + 10,
+                  titlesData: const FlTitlesData(
+                      leftTitles: AxisTitles(
+                          axisNameWidget: Text('Values ->'),
+                          sideTitles: SideTitles(showTitles: true)),
+                      bottomTitles: AxisTitles(
+                          axisNameWidget: Text('Time ->'),
+                          sideTitles: SideTitles(showTitles: true))),
+                  lineBarsData: [
+                    LineChartBarData(
+                        spots: dataPoints,
+                        isCurved: true,
+                        color: Colors.red,
+                        dotData: const FlDotData(show: true)),
+                    LineChartBarData(
+                        spots: meanPoints,
+                        isCurved: true,
+                        color: Colors.orange,
+                        dotData: const FlDotData(show: true)),
+                    LineChartBarData(
+                        spots: variancePoints,
+                        isCurved: true,
+                        color: Colors.blue,
+                        dotData: const FlDotData(show: true))
+                  ]))),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                  'x=> ${_gyroscopeEvents.last.x.toStringAsFixed(2)}, y=> ${_gyroscopeEvents.last.y.toStringAsFixed(2)}, z=> ${_gyroscopeEvents.last.z.toStringAsFixed(2)}'),
+              Text(
+                  'Main Value : ${dataPoints.isNotEmpty ? dataPoints.last.y.toStringAsFixed(2) : 'N/A'}',
+                  style: const TextStyle(color: Colors.red)),
+              Text(
+                  'Mean Value : ${meanPoints.isNotEmpty ? meanPoints.last.y.toStringAsFixed(2) : 'N/A'}',
+                  style: const TextStyle(color: Colors.orange)),
+              Text(
+                  'Variance Value : ${variancePoints.isNotEmpty ? variancePoints.last.y.toStringAsFixed(2) : 'N/A'}',
+                  style: const TextStyle(color: Colors.blue)),
+            ],
+          ),
+        ),
+        if (showLoader)
+          Center(
+            child: Container(
+              width: 10,
+              height: 10,
+              child: const CircularProgressIndicator(),
+            ),
+          ),
+      ]),
     );
   }
 }
